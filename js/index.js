d417953a4df8c1456a62a466b76b9082a65ee362
Humberto <humbertolima108@gmail.com>
2023-04-18 11:47:06 -0300

    adicionando js na parte 2

--- js/index.js
+++ js/index.js
@@ -7,10 +7,27 @@ const inputSearch = document.querySelector('.input-search');
 const buttonNext = document.querySelector('.button-next');
 const buttonBack = document.querySelector('.button-back');
 
-let searchPokemon = 1; // essa variável é o contador da pokedex, como tem o valor 1, vai começar pelo pokemon 1, bulbasaur
+
+const pokemonType1 = document.querySelector('.pokemon-type1')
+const pokemonType2 = document.querySelector('.pokemon-type2')
+const pokemonHeight = document.querySelector('.pokemon-height')
+const pokemonWeight = document.querySelector('.pokemon-weight')
+
+const pokemonSpriteFront = document.querySelector('.pokemon-sprite-9-front')
+const pokemonSpriteBack = document.querySelector('.pokemon-sprite-9-back')
+
+function zeroLeft(num){
+  if(num<10){
+      return ('0.'+num)
+  }else{
+      return (num)
+  }
+}
+
+var counterPokemon = 1; // essa variável é o contador da pokedex, como tem o valor 1, vai começar pelo pokemon 1, bulbasaur
 
 const fetchPokemon = async (pokemon) => {
-  // função para utilizar a api pokemon, nessa funçao eu vou receber um valor, sejá id ou name do pokemon e ali em baixo vai retornar os dados dele
+
   const APIResponse = await fetch(
     `https://pokeapi.co/api/v2/pokemon/${pokemon}`
   );
@@ -27,6 +44,12 @@ const renderPokemon = async (pokemon) => {
   pokemonName.innerHTML = 'Loading...';
   pokemonNumber.innerHTML = ' ';
   pokemonImg.src = '#';
+  pokemonType1.innerHTML = ''
+  pokemonType2.innerHTML = ''
+  pokemonHeight.innerHTML = ''
+  pokemonWeight.innerHTML =''
+  pokemonSpriteFront.src = '#'
+  pokemonSpriteBack.src = '#'
 
   const data = await fetchPokemon(pokemon); 
   if (data) {
@@ -36,9 +59,21 @@ const renderPokemon = async (pokemon) => {
     pokemonImg.src =
       data['sprites']['versions']['generation-v']['black-white']['animated'][
         'front_default'
-      ]; // logo aqui vamos atualizar o valor src com o a informação da api(Em uma atualização futura colocarei um if para os pokemons cujo o id seja 649<, aí ele vai pegar o sprite da generation-viii)
+      ]; 
+    pokemonType1.innerHTML = data['types']['0']['type']['name']
+    pokemonType2.innerHTML = data['types']['1']['type']['name']
 
-    searchPokemon = data.id; // isso aqui serve para atualizar o nosso valor para os botões de avançar e voltar funcionarem corretamente, exemplo, vc pesquisa o pokemon 25, pikachu, aí vai apertar avançar e vai pro raichu 26, sem isso ele iria pro número 2, ivysaur
+    const pokeHeight = data.height
+    const pokeWeight = data.weight
+    pokemonHeight.innerHTML = zeroLeft(pokeHeight)+'M'
+    pokemonWeight.innerHTML =zeroLeft(pokeWeight)+'KG'
+
+    pokemonSpriteFront.setAttribute('src','#')
+    pokemonSpriteFront.src =data['sprites']['versions']['generation-vii']['ultra-sun-ultra-moon']['front_default']
+    pokemonSpriteBack.setAttribute('src','#')
+    pokemonSpriteBack.src = data['sprites']['versions']['generation-vii']['ultra-sun-ultra-moon']['front_shiny']
+
+    counterPokemon = data.id; 
     pokemonImg.style.bottom = '47%';
     pokemonImg.style.height = '18%'; // essa foi a minha maior gambiarra para os sprites não bugarem por conta da imagem de não encontrado
     pokemonImg.style.width = '30%';
@@ -66,13 +101,13 @@ form.addEventListener('submit', (event) => {
 
 buttonNext.addEventListener('click', () => {
   
-  searchPokemon += 1; 
-  renderPokemon(searchPokemon); 
+  counterPokemon += 1; 
+  renderPokemon(counterPokemon); 
 });
 buttonBack.addEventListener('click', () => {
-  if (searchPokemon > 1) {
-    searchPokemon -= 1;
-    renderPokemon(searchPokemon);
+  if (counterPokemon > 1) {
+    counterPokemon -= 1;
+    renderPokemon(counterPokemon);
   }
 });
-renderPokemon(searchPokemon);
+renderPokemon(counterPokemon);
